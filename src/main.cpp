a834367f2908a238a1406ac31b196399d3d7e646
atplab <154367090+atplab@users.noreply.github.com>
2025-10-07 17:44:02 -0400

    cours7_final

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,65 @@
 #include <Arduino.h>
+#include <FastLED.h>
+#include <M5_PbHub.h>
+M5_PbHub myPbHub;
 
-// put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#define BROCHE_ATOM_FIL_BLANC 32
+#define BROCHE_ATOM_FIL_JAUNE 26
+#define BROCHE_ATOM_LUMIERE 27
+#define BROCHE_ATOM_BOUTON 39
+#define KEY_CHANNEL 0
+//CRGB keyPixel;
+CRGB monPixel;
+
+void setup()
+{
+  // Initialiser FastLED pour contrôler le pixel RGB du M5Stack Key Unit
+  //FastLED.addLeds<WS2812, BROCHE_ATOM_FIL_JAUNE, GRB>(&keyPixel, 1);
+  //pinMode(BROCHE_ATOM_FIL_BLANC, INPUT_PULLUP);
+  FastLED.addLeds<WS2812, BROCHE_ATOM_LUMIERE, GRB>(&monPixel, 1);
+  pinMode(BROCHE_ATOM_BOUTON, INPUT);
+
+  Wire.begin();
+  myPbHub.begin();
+  myPbHub.setPixelCount( KEY_CHANNEL ,  1 );
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  int maLectureKey = myPbHub.digitalRead( KEY_CHANNEL );
+  
+
+
+  //int maLectureKey = digitalRead(BROCHE_ATOM_FIL_BLANC);
+
+  if (maLectureKey == 0)
+  {
+    myPbHub.setPixelColor(KEY_CHANNEL, 0, 127, 255, 0);
+    //keyPixel = CRGB(0, 255, 255);
+    
+  }
+
+  else
+  {
+    myPbHub.setPixelColor(KEY_CHANNEL, 0, 0, 0, 0);
+    //keyPixel = CRGB(255, 255, 0);
+    
+  }
+
+  int maLectureLumiere = digitalRead(BROCHE_ATOM_BOUTON);
+
+  if (maLectureLumiere == 0)
+  {
+    
+    monPixel = CRGB(255, 255, 255);
+  }else
+  {
+    
+    monPixel = CRGB(0, 0, 0);
+  }
+  FastLED.show();
+  delay(20);
 }
